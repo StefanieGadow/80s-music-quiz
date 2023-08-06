5e1b2b7095fd766bad6101c22ca851a4baadc883
Stefanie Gadow <StefanieGadow@posteo.de>
2023-08-06 08:06:26 +0000

    Feat: Add main_quiz to randomize questions and get the user's guess

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import random
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -49,14 +50,43 @@ def get_questions_from_sheet(data):
     """
     Fetch questions and correct answers from a Google Sheets document.
     """
-    question_answer_pairs = [(row[0], row[5]) for row in data[1:]]
+    question_answer_pairs = [(row[0], row[5], row[1:5]) 
+    for row in data[1:] if row[0] and row[5]]
+    # print(question_answer_pairs)
+    return question_answer_pairs
 
-    print(question_answer_pairs)
+
+def main_quiz(question_answer_pairs):
+    score = 0
+    num_questions = 1
+    questions_to_ask = random.sample(question_answer_pairs, num_questions)
+    for question, correct_answer, possible_answers in questions_to_ask:
+        # possible_answers = question[1:5]
+        # correct_answer = question[5]
+
+        print(question)
+        num_questions += 1
+
+        random.shuffle(possible_answers)
+
+        for i, ans in enumerate(possible_answers, 1):
+            print(f"{i}. {ans}")
+
+        user_answer = int(input("Your answer (enter the option numer): "))
+
+        if user_answer == possible_answers.index(correct_answer) + 1:
+            print("Correct!")
+            score += 1
+        else:
+            print(f"The correct answer is: {correct_answer}")
+        print("----------------------------------------------------")
+    return score
 
 
 def main():
     print_logo()
     display_rules()
-    get_questions_from_sheet(data);
+    question_answer_pairs = get_questions_from_sheet(data);
+    score = main_quiz(question_answer_pairs)
 
 main()
